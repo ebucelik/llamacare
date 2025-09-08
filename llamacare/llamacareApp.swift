68c45f55406f3c8004c58e536bee99e39cb85675
Ing. Ebu Celik <ebucelik1@hotmail.com>
2025-09-08 20:58:04 +0200

    Initial Commit

---
+++ llamacare/llamacareApp.swift
@@ -0,0 +1,17 @@
+//
+//  llamacareApp.swift
+//  llamacare
+//
+//  Created by Ing. Ebu Bekir Celik, BSc, MSc on 08.09.25.
+//
+
+import SwiftUI
+
+@main
+struct llamacareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
